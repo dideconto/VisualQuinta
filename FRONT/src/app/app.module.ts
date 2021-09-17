a2c0ee909ef065fafd872f659dafbc9365164bbd
Diogo Deconto <dideconto@gmail.com>
2021-09-16 22:06:47 -0300

    Cadastro do produto e redirecionamento de componentes

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
-import { ProdutoCadastrarComponent } from './components/views/produto/produto-cadastrar/produto-cadastrar.component';
-import { ProdutoListarComponent } from './components/views/produto/produto-listar/produto-listar.component';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+//Imports dos componentes
+import { AppComponent } from "./app.component";
+import { ProdutoCadastrarComponent } from "./components/views/produto/produto-cadastrar/produto-cadastrar.component";
+import { ProdutoListarComponent } from "./components/views/produto/produto-listar/produto-listar.component";
 
 @NgModule({
-    declarations: [AppComponent, ProdutoCadastrarComponent, ProdutoListarComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    declarations: [
+        AppComponent,
+        ProdutoCadastrarComponent,
+        ProdutoListarComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- FRONT/src/app/components/views/produto/produto-cadastrar/produto-cadastrar.component.ts
+++ FRONT/src/app/components/views/produto/produto-cadastrar/produto-cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Produto } from "src/app/models/Produto";
+import { ProdutoService } from "src/app/services/produto.service";
 
 @Component({
     selector: "app-produto-cadastrar",
@@ -6,8 +9,26 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./produto-cadastrar.component.css"],
 })
 export class ProdutoCadastrarComponent implements OnInit {
-    constructor() {}
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
+    nome!: string;
+    descricao!: string;
+    preco!: number;
+    quantidade!: number;
+
+    constructor(private service: ProdutoService, private router: Router) {}
+
+    ngOnInit(): void {}
+
+    cadastrar(): void {
+        let produto: Produto = {
+            nome: this.nome,
+            descricao: this.descricao,
+            quantidade: this.quantidade,
+            preco: this.preco,
+        };
+        //Chamar o create do service
+        this.service.create(produto).subscribe((produto) => {
+            console.log(produto);
+            this.router.navigate(["produto/listar"]);
+        });
     }
 }
