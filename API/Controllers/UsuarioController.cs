e58925c8646d20608cb28e1138b060136909fe80
Diogo Deconto <dideconto@gmail.com>
2021-11-18 21:37:36 -0300

    Implementação da autenticação e autorização por JWT na API - Parte II

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using API.Data;
 using API.Models;
+using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,11 @@ namespace API.Controllers
         [Route("create")]
         public IActionResult Create([FromBody] Usuario usuario)
         {
+            Usuario usuarioExistente = _context.Usuarios.FirstOrDefault(u => u.Email == usuario.Email);
+            if (usuarioExistente != null)
+            {
+                return BadRequest(new { message = "Usuário já cadastrado" });
+            }
             _context.Usuarios.Add(usuario);
             _context.SaveChanges();
             usuario.Senha = "";
@@ -32,7 +38,42 @@ namespace API.Controllers
         [Route("login")]
         public IActionResult Login([FromBody] Usuario usuario)
         {
+            usuario = _context.Usuarios.FirstOrDefault
+                (u => u.Email == usuario.Email && u.Senha == usuario.Senha);
+            if (usuario == null)
+            {
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+            }
+            usuario.Token = TokenService.CriarToken(usuario);
+            usuario.Senha = "";
             return Ok(usuario);
+        }
+
+        // GET: api/usuario/sem
+        [HttpGet]
+        [Route("sem")]
+        [AllowAnonymous]
+        public IActionResult Sem()
+        {
+            return Ok(new { message = "Sem autenticação" });
+        }
+
+        // GET: api/usuario/com
+        [HttpGet]
+        [Route("com")]
+        [Authorize]
+        public IActionResult Com()
+        {
+            return Ok(new { message = User.Identity.Name });
+        }
+
+        // GET: api/usuario/permissao
+        [HttpGet]
+        [Route("permissao")]
+        [Authorize(Roles = "adm")]
+        public IActionResult Permissao()
+        {
+            return Ok(new { message = "ADM" });
         }
 
     }
---
+++ API/Settings.cs
@@ -0,0 +1,7 @@
+namespace API
+{
+    public static class Settings
+    {
+        public static string Secret = "r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&E";
+    }
+}
